30e2065ce475da7e08ae6bc20f6a3c0a6640c54a
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2022-10-13 13:49:09 -0500

    Estructura Basica ,B hijo de A

---
+++ main.cc
@@ -0,0 +1,23 @@
+/*
+class A { public void foo(){ System.out.println("A"); }}
+class B extends A { public void foo(){ System.out.println("B");}}
+class C extends A { public void goo(){ System.out.println("C");}}
+class D extends B { public void goo(){ System.out.println("D");}}
+class E extends C { public void foo(){ System.out.println("E");}}
+
+
+B y C son hijos directos de A
+
+D hijo de B
+E hijo de C
+
+*/
+
+#include "A.h"
+#include "B.h"
+
+int main(){
+
+
+
+}
